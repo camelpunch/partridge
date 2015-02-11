62d1757ed6db930818edc35da7a6814536e764f0
Andrew Bruce <andrew@camelpunch.com>
2015-02-11 22:13:59 +0000

    Follow reagent google maps tutorial
    Available here: https://github.com/reagent-project/reagent-cookbook/tree/master/recipes/google-maps
    
    Adapt for...domain.

--- src/partridge/core.cljs
+++ src/partridge/core.cljs
@@ -1,17 +1,26 @@
 (ns ^:figwheel-always partridge.core
     (:require
-              [reagent.core :as reagent :refer [atom]]))
+     [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
-
-(println "Edits to this text should show up in your developer console.")
 
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
+(defn home []
+  [:div [:h1 "Waterway to explore the canals!"]
+   [:div#map-canvas]])
 
-(reagent/render-component [hello-world]
+(defn home-did-mount []
+  (let [map-canvas (.getElementById js/document "map-canvas")
+        map-options (clj->js {"center" (google.maps.LatLng. -34.397 150.644)
+                              "zoom" 8})]
+    (js/google.maps.Map. map-canvas map-options)))
+
+(defn home-component []
+  (reagent/create-class {:render home
+                         :component-did-mount home-did-mount}))
+
+(reagent/render-component [home-component]
                           (. js/document (getElementById "app")))
