29a65aca6ee9b7db0b1a6fd0aac75b5f80bbea58
Andrew Bruce <andrew@camelpunch.com>
2015-02-11 22:26:56 +0000

    Focus on Nantwich, hardcode a select element.

--- src/partridge/core.cljs
+++ src/partridge/core.cljs
@@ -4,18 +4,19 @@
 
 (enable-console-print!)
 
-;; define your app data so that it doesn't get over-written on reload
-
 (defonce app-state (atom {:text "Hello world!"}))
 
 (defn home []
-  [:div [:h1 "Waterway to explore the canals!"]
+  [:div
+   [:div#controls
+    [:label {:for "waterway"} "Waterway:"]
+    [:select#waterway [:option "Trent & Mersey Canal"]]]
    [:div#map-canvas]])
 
 (defn home-did-mount []
   (let [map-canvas (.getElementById js/document "map-canvas")
-        map-options (clj->js {"center" (google.maps.LatLng. -34.397 150.644)
-                              "zoom" 8})]
+        map-options (clj->js {:center {:lat 53.0672367 :lng -2.52393}
+                        :zoom 9})]
     (js/google.maps.Map. map-canvas map-options)))
 
 (defn home-component []
